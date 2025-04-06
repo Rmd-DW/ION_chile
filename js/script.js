eda0902c06c7aefc37fba30706ec8bc44e632514
Rmd-DW <r.munizaga.d@gmail.com>
2025-04-06 10:23:19 -0400

    actualización Navbar

--- js/script.js
+++ js/script.js
@@ -54,3 +54,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+  navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+      navLinks.forEach(l => l.classList.remove("active"));
+      link.classList.add("active");
+    });
+  });
+});
